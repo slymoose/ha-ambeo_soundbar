2d83810afcaaade622cb7eb80029c4f350e9cc7f
slymoose <jmoorse@gmail.com>
2024-09-17 20:26:09 -0600

    Adding buttons for wake and sleep.

--- custom_components/ambeo_soundbar/button.py
+++ custom_components/ambeo_soundbar/button.py
@@ -26,6 +26,47 @@ class AmbeoReboot(AmbeoBaseEntity, ButtonEntity):
     def device_class(self) -> ButtonDeviceClass:
         """Return the device class."""
         return ButtonDeviceClass.RESTART
+
+
+class AmbeoWake(AmbeoBaseEntity, ButtonEntity):
+    """Wakes up the Ambeo."""
+    def __init__(self, device, api):
+        """Initialize the switch entity."""
+        super().__init__(device, api, "Ambeo Wake", "ambeo_wake")
+
+    async def async_press(self) -> None:
+        """Handle the button press."""
+        await self.api.wake();
+
+    @property
+    def entity_category(self) -> EntityCategory:
+        """Return the entity category."""
+        return EntityCategory.CONFIG
+
+    @property
+    def device_class(self) -> ButtonDeviceClass:
+        """Return the device class."""
+        return ButtonDeviceClass.RESTART
+
+class AmbeoStandby(AmbeoBaseEntity, ButtonEntity):
+    """Staby mode for the Ambeo."""
+    def __init__(self, device, api):
+        """Initialize the switch entity."""
+        super().__init__(device, api, "Ambeo Standby", "ambeo_standby")
+
+    async def async_press(self) -> None:
+        """Handle the button press."""
+        await self.api.stand_by();
+
+    @property
+    def entity_category(self) -> EntityCategory:
+        """Return the entity category."""
+        return EntityCategory.CONFIG
+
+    @property
+    def device_class(self) -> ButtonDeviceClass:
+        """Return the device class."""
+        return ButtonDeviceClass.RESTART
     
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -35,5 +76,9 @@ async def async_setup_entry(
     """Set up the switch entities from a config entry created in the integrations UI."""
     ambeo_api = hass.data[DOMAIN][config_entry.entry_id]["api"]
     ambeo_device = hass.data[DOMAIN][config_entry.entry_id]["device"]
-    entities = [AmbeoReboot(ambeo_device, ambeo_api)]
+     entities = []
+    entities.append(AmbeoReboot(ambeo_device, ambeo_api))
+    entities.append(AmbeoWake(ambeo_device, ambeo_api))
+    entities.append(AmbeoStandby(ambeo_device, ambeo_api))
+    async_add_entities(entities) #may need (entities, update_before_add=True)
     async_add_entities(entities)
